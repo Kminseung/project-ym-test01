4c7953897e9590b3590bfd074dcc10179db253d7
Kminseung <soccer6504@gmail.com>
2021-06-14 17:34:46 +0900

    Board Register, Modify, Delete

---
+++ src/test/java/com/example/springboot/web/IndexControllerTest.java
@@ -0,0 +1,29 @@
+package com.example.springboot.web;
+
+import com.example.springboot.web.dto.PostsResponseDto;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void mainPage_Loading() {
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        assertThat(body).contains("Welcome to YourMine Market");
+    }
+}
---
+++ src/test/java/com/example/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.example.springboot.web.domain.posts;
+
+import com.example.springboot.domain.posts.Posts;
+import com.example.springboot.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void loading() {
+        String title = "Test Board";
+        String content = "Test Content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("hong@gildong.com")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_Register() {
+        LocalDateTime now = LocalDateTime.of(2021, 6, 14, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+}
